5b9cd60670d0f6fe03a79db161936cd5d6cf9131
solji <123902525+sooolji@users.noreply.github.com>
2025-12-30 20:52:10 -0500

    Initial commit: Add program.cs with project outline

---
+++ program.cs
@@ -0,0 +1,6 @@
+// github-activity <sooolji> formato del cli
+// devolver la actividad en la terminal
+// tratar las excepcions
+// tratar respuestas indevidas de la API
+// don't use any external libraries or frameworks to fetch
+
